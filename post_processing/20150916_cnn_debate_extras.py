8a4415b0625af9792f34a49816d5c39ed2bb6014
Timothy Renner <renner.timothy@gmail.com>
2015-09-16 17:09:52 -0500

    Prepped post processing scripts for the CNN debate.

---
+++ post_processing/20150916_cnn_debate_extras.py
@@ -0,0 +1,25 @@
+import csv
+
+candidates = ["Chris Christie", "Rand Paul", "Mike Huckabee",
+              "Scott Walker", "Donald Trump", "Jeb Bush",
+              "Ben Carson", "Ted Cruz", "Marco Rubio",
+              "John Kasich", "Carly Fiorina"]
+words = ["fuck", "shit", "ass", "bitch", "douche", "dick", "damn"]
+times = ["2015-09-16T19:%02d-0400"%(i) for i in range(30,60)] + \
+        ["2015-09-16T20:%02d-0400"%(i) for i in range(60)] + \
+        ["2015-09-16T21:%02d-0400"%(i) for i in range(60)] + \
+        ["2015-09-16T22:%02d-0400"%(i) for i in range(60)] +\
+        ["2015-09016T23:%02d-0400"%(i) for i in range(30)]
+
+data = [{'subject': s, 'word': w, 'time': t, 'count': 0}
+        for s in candidates
+        for w in words
+        for t in times]
+        
+with open("20150916_cnn_debate_extras.csv","w") as out:
+    fieldnames = ["subject", "word", "time", "count"]
+    
+    writer = csv.DictWriter(out, fieldnames=fieldnames, delimiter="\t")
+    for row in data:
+        writer.writerow(row)
+    
