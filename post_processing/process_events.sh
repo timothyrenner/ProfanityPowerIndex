bb23f4ea5449cacb098dd4ea184f33d3714564ed
Timothy Renner <renner.timothy@gmail.com>
2015-09-30 17:32:24 -0500

    Replaced SQL-based counting with a generic shell script.
    Removed the SQL scripts for the CNN and Fox News debates as they
    are redundant.

--- post_processing/process_events.sh
+++ post_processing/process_events.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+real_data=$1
+jump_data=$2
+
+# Read the start and stop times from the jumpstarted data.
+start_time=$(cat $jump_data | head -n 1 | csvcut -tc 3)
+stop_time=$(cat $jump_data | tail -n 1 | csvcut -tc 3)
+
+sqlite3 ppi.db <<EOF
 -- Import the raw data.
 DROP TABLE IF EXISTS raw_data;
 CREATE TABLE raw_data (tweet_id TEXT,
@@ -5,43 +15,41 @@ CREATE TABLE raw_data (tweet_id TEXT,
                        created_at TEXT,
                        subject TEXT,
                        word TEXT);
-
 .separator \t
-.import 20150806_fn_debate.csv raw_data
+.import ${real_data} raw_data
 
 -- Reduce with counts.
 DROP TABLE IF EXISTS raw_counts;
 CREATE TABLE raw_counts AS
-SELECT subject, word, created_at as time, count(*) as count
+SELECT subject, word, created_at AS time, COUNT(*) AS COUNT
 FROM raw_data
 GROUP BY subject, word, time;
 
--- Manually insert all of the word, candidate combinations for the collect
--- start time and collect end time.
+-- Insert the jumpstart data.
 DROP TABLE IF EXISTS counts;
-CREATE TABLE counts (subject TEXT, 
+CREATE TABLE counts (subject TEXT,
                      word TEXT,
                      time TEXT,
                      count INTEGER);
-   
 .separator \t
-.import 20150806_fn_debate_extras.csv counts
-   
--- Insert the (filtered) collected data into the table.
-INSERT INTO counts 
+.import ${jump_data} counts
+
+-- Insert the collected data into the count table.
+INSERT INTO counts
 SELECT * FROM raw_counts
-WHERE time >= '2015-08-06T20:30-0400';
-   
--- Finally, reapply the groupby and sort.
+WHERE time >= '${start_time}' AND
+      time <= '${stop_time}';
+
+-- Finally, reapply the reduction and sort.
 DROP TABLE IF EXISTS final_counts;
 CREATE TABLE final_counts AS
-SELECT subject, word, time, sum(count) as count
+SELECT subject, word, time, SUM(count) AS count
 FROM counts
 GROUP BY subject, word, time
 ORDER BY time, subject, word;
-   
+
 -- Export.
 .headers on
-.output 20150806_fn_debate_counts.csv
 
 SELECT * FROM final_counts;
+EOF
