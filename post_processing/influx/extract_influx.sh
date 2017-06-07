6fd678be78a47d498be057d5125232891721f13d
Tim Renner <renner.timothy@gmail.com>
2017-06-06 21:01:04 -0500

    Removed legacy post processing code.

--- post_processing/influx/extract_influx.sh
+++
@@ -1,7 +0,0 @@
-START_TIME="'$(gdate -d $1 -u --iso-8601=seconds)'"
-STOP_TIME="'$(gdate -d $2 -u --iso-8601=seconds)'"
-
-influx -database 'profanity' \
--execute "SELECT COUNT(id) AS count FROM profanity WHERE time >= $START_TIME AND time <= $STOP_TIME GROUP BY subject, word, time(1m) FILL(0)" \
--format csv \
--precision rfc3339 | awk -f process_influx.awk
